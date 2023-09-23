30635e3b20d9f5d574fd028be0d0aaf43c6bb686
Ay2416 <147193081+Ay2416@users.noreply.github.com>
2023-09-23 21:42:48 +0700

    Update main.py (v1.1)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import glob
+import ndjson
 
 # my program import
 # none
@@ -17,10 +18,12 @@ load_dotenv()
 
 # 変数
 intents = discord.Intents.default()
+intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 data_location = "./data"
 
+# Bot起動時に行う処理
 @bot.event
 async def on_ready():
     print("接続しました！")
@@ -31,8 +34,8 @@ async def on_ready():
     await bot.load_extension("Cogs.Point")
     await bot.load_extension("Cogs.Other")
 
-    await bot.tree.sync()
-    print("グローバルコマンド同期完了！")
+    #await bot.tree.sync()
+    #print("グローバルコマンド同期完了！")
 
     #await bot.tree.sync(guild=discord.Object(your_guild_id))
     #print("ギルドコマンド同期完了！")
@@ -82,5 +85,55 @@ async def on_guild_remove(guild):
     if judge == 1:
         os.remove(data_location + "/guild_json/" + str(guild.id))
         print("キックまたはBANされたため、" + str(guild.id) + "のguild jsonを削除しました。")
+
+# サーバーからユーザーが脱退した場合に、そのユーザーのポイントデータを削除する処理
+@bot.event
+async def on_member_remove(member):
+        # このサーバーのデータがあるかどうかの確認
+        files = glob.glob(data_location + '/guild_json/*.ndjson')
+        judge = 0
+
+        for i in range(0, len(files)):
+            #print(os.path.split(files[i])[1])
+            if(os.path.split(files[i])[1] == str(member.guild.id) + ".ndjson"):
+                judge = 1
+                break
+        
+        if judge != 1:
+            print("エラー!：このサーバーにはポイントデータがありません。")
+            return
+
+        # ユーザーのポイントデータがあるかどうかを確認
+        with open(data_location + '/guild_json/' + str(member.guild.id) + '.ndjson') as f:
+            read_data = ndjson.load(f)
+        
+        judge = 0
+        location = 0
+        for i in range(0, len(read_data)):
+                if read_data[i]["user"] == member.id:
+                    judge = 1
+                    location = i
+                    break
+        
+        if judge != 1:
+            print("エラー!：そのユーザーのポイントデータがありません。")
+            return
+
+        # もしサーバーのデータの個数が1つしかなければ、ファイルごと削除する ※その場合は、ここで処理を終了させる
+        if len(read_data) == 1:
+            os.remove(data_location + '/guild_json/' + str(member.guild.id) + '.ndjson')
+            print("成功しました！：<@" + str(member.id) + ">さんのポイントデータを削除しました。")
+            return
+
+        # そのユーザーのデータを削除
+        os.remove(data_location + '/guild_json/' + str(member.guild.id) + '.ndjson')
+
+        for i in range(0, len(read_data)):
+            if i != location:
+                with open(data_location + '/guild_json/' + str(member.guild.id) + '.ndjson', 'a') as f:
+                    writer = ndjson.writer(f)
+                    writer.writerow(read_data[i])
+        
+        print("成功しました！：<@" + str(member.id) + ">さんのポイントデータを削除しました。")
 
 bot.run(os.environ['token'])
